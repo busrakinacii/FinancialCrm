2be0bc7d468e1d117b2872172aa30eae83676e10
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2025-02-17 23:23:42 +0300

    Changed Added

---
+++ FinancialCrm/FrmBilling.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FinancialCrm.Models;
+
+namespace FinancialCrm
+{
+    public partial class FrmBilling : Form
+    {
+        public FrmBilling()
+        {
+            InitializeComponent();
+        }
+        FinancialCrmDbEntities db = new FinancialCrmDbEntities();
+        private void FrmBilling_Load(object sender, EventArgs e)
+        {
+            var values = db.Bills.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnBillList_Click(object sender, EventArgs e)
+        {
+            var values = db.Bills.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnCreateBill_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
