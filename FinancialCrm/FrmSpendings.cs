864e26cac6774dc249aa805daaf54321d3b4204e
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2025-02-25 23:26:55 +0300

    changed Added

--- FinancialCrm/FrmSpendings.cs
+++ FinancialCrm/FrmSpendings.cs
@@ -24,10 +24,8 @@ namespace FinancialCrm
             FrmDashboard fr = new FrmDashboard();
             fr.Show();
         }
-
-        private void btnSpendingList_Click(object sender, EventArgs e)
+        void spendingList()
         {
-
             //spendings List Process
             //
             var spendingList = db.Spendings.Select(x => new
@@ -39,25 +37,40 @@ namespace FinancialCrm
                 x.Categories.CategoryName
             }).ToList();
             dataGridView1.DataSource = spendingList;
+        }
+        private void btnSpendingList_Click(object sender, EventArgs e)
+        {
+            spendingList();
         }
 
         private void FrmSpendings_Load(object sender, EventArgs e)
         {
-            //spendings List Process
-            //
-            var spendingList = db.Spendings.Select(x => new
-            {
-                x.SpendingId,
-                x.SpendingTitle,
-                x.SpendingAmount,
-                x.SpendingDate,
-                x.Categories.CategoryName
-            }).ToList();
-            dataGridView1.DataSource = spendingList;
+            spendingList();
+            cmbCategory.DisplayMember = "CategoryName";
+            cmbCategory.ValueMember = "CategoryId";
+            cmbCategory.DataSource = db.Categories.ToList();
         }
 
         private void btnCreateSpending_Click(object sender, EventArgs e)
         {
+            //string spendingTitle = txtSpendingTitle.Text;
+            //decimal spendingAmount = decimal.Parse(txtSpendingAmount.Text);
+            //DateTime spendingDate = DateTime.Parse(txtSpendingDate.Text);
+            //int categoryID = int.Parse(cmbCategory.SelectedValue.ToString());
+
+            //Spendings spendings = new Spendings();
+            //spendings.SpendingTitle = spendingTitle;
+            //spendings.SpendingAmount = spendingAmount;
+            //spendings.SpendingDate = spendingDate;
+            //spendings.CategoryId = categoryID;
+
+            //db.Spendings.Add(spendings);
+            //db.SaveChanges();
+            //MessageBox.Show("Giderler Başarılı Bir Şekilde Sisteme Eklendi.", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            ////spendings List Process
+            ////
+            //spendingList();
 
         }
     }
